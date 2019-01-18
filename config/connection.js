107e5aae155c50617808b36309320dd212fe7908
rkoven74 <roxannekoven@gmail.com>
2019-01-17 21:59:29 -0700

    up to date

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,28 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
-console.log('LOOOOK HERE', process.env.JAWSDB_URL);
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
+// // Set up MySQL connection.
+// var mysql = require("mysql");
+// var connection;
+// console.log('LOOOOK HERE', process.env.JAWSDB_URL);
+// if(process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+//   });
+// }
 
 
-// Make connection.
-connection.connect(function(err) {
-  if (err)throw err; {
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err)throw err; {
    
-  console.log("connected as id " + connection.threadId);
-  }
-});
+//   console.log("connected as id " + connection.threadId);
+//   }
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
